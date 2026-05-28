941c2b53bbe0c860dd04cd24132175ed403d62f4
Boris Yajamin <boradmy@gmail.com>
2026-05-28 03:08:12 -0500

    complete 1

--- app/src/main/java/ec/edu/uisek/githubclient/services/RetrofitClient.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/services/RetrofitClient.kt
@@ -5,7 +5,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 object RetrofitClient{
     private const val BASE_URL = "https://api.github.com/"
 
-    private  val loggin = HttpLoggingInterceptor
+    private  val loggin = HttpLoggingInterceptors
 
 
 }
