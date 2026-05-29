e3a52772a68233f485f858dbf769cb71a0db67db
Boris Yajamin <boradmy@gmail.com>
2026-05-29 01:05:55 -0500

    2154

--- app/src/main/java/ec/edu/uisek/githubclient/services/ApiService.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/services/ApiService.kt
@@ -2,9 +2,15 @@ package ec.edu.uisek.githubclient.services
 
 import ec.edu.uisek.githubclient.models.Repository
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
-
     @GET(value = "user/repos")
-    suspend fun getRepositories(): List<Repository>
+    suspend fun getRepositories(
+        @Query(value = "sort") sort: String = "created",
+        @Query(value = "direction") direction: String = "desc",
+        @Query(value = "per_page") perPage: Int = 100,
+        @Query(value = "affiliation") affiliation: String = "owner",
+        @Query(value = "t") t: String = "${System.currentTimeMillis()}"
+    ): List<Repository>
 }
